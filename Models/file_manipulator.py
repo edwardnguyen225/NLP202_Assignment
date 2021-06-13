bdd1bfdf3164e4836ac204d4314ddc2eab3a95d1
Nhan Nguyen <trinhan194@gmail.com>
2021-06-13 18:20:27 +0700

    Add write_malt_to_string; Fix not write in utf-8

--- Models/file_manipulator.py
+++ Models/file_manipulator.py
@@ -1,3 +1,30 @@
+import json
+
+
 def write_file(path_to_file, data):
-    with open(path_to_file, "w") as f:
+    try:
+        f = open(path_to_file, "w", encoding='utf-8')
         f.write(data)
+        f.close()
+    except IOError:
+        raise IOError
+
+
+def relation_to_string(relation):
+    left, right = relation
+    str = f'({left}, {right})'
+    return str
+
+
+def malt_to_string(malt):
+    result = ""
+    for relation in malt:
+        row = relation_to_string(relation)
+        row += f': {malt[relation]}\n'
+        result += row
+    return result
+
+
+def write_malt_to_file(path_to_file, malt):
+    data = malt_to_string(malt)
+    write_file(path_to_file, data)
