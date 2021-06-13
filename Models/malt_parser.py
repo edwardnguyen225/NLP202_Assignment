1a466cf92bb1062ab14fa9a4fc73acd7d442500e
Nhan Nguyen <trinhan194@gmail.com>
2021-06-13 11:21:04 +0700

    Add basic funcs, handle_splittingxebus

---
+++ Models/malt_parser.py
@@ -0,0 +1,26 @@
+import re
+from copy import copy
+from pyvi import ViTokenizer
+
+
+def malt_parser(str, my_lexicals, rules):
+    malt = {}
+    sentence = copy(str)
+    tokens = ViTokenizer.tokenize(sentence).lower().split()
+    print(tokens)
+    handle_splitting_xebus(tokens)
+    print(tokens)
+
+    return malt
+
+
+def handle_splitting_xebus(tokens):
+    for i in range(len(tokens) - 2):
+        if tokens[i] == "xe" and tokens[i+1] == "bus":
+            tokens[i] = "xe_bus"
+            tokens.pop(i+1)
+    return tokens
+
+
+def get_token_type(word, my_lexicals):
+    return None
