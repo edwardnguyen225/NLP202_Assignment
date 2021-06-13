741a4ecd63a854e6fc10ed47452d63d530398afc
Nhan Nguyen <trinhan194@gmail.com>
2021-06-13 18:22:29 +0700

    Add functions & Finish malt parser

--- Models/malt_parser.py
+++ Models/malt_parser.py
@@ -3,6 +3,10 @@ from pyvi import ViTokenizer
 
 
 ROOT = "<ROOT>"
+LEFTARC = "LEFT-ARC"
+RIGHTARC = "RIGHT-ARC"
+REDUCE = "REDUCE"
+SHIFT = "SHIFT"
 
 
 def handle_splitting_xebus(tokens):
@@ -23,20 +27,40 @@ class MaltParser():
     def __init__(self, str, my_lexicals, rules):
         self.str = str
         self.my_lexicals = my_lexicals
-        self.rules = rules
+        self.dependency_relations = rules
         self.tokens = tokenize(self.str)
         self.malt = {}
 
     def parse(self):
-        buffer = self.tokens
-        stack = [ROOT]
-        malt = {}
+        self.stack = [ROOT]
+        self.buffer = self.tokens
+        self.malt = {}
 
-        tokens_type = list(
-            map(lambda x: self.get_token_type(x), buffer))
+        # count = 0
+        # print(self.buffer)
+        tokens_type = list(map(lambda x: self.get_token_type(x), self.buffer))
+        # print((tokens_type))
+        # print(self.stack)
+        while self.buffer:
+            # print("\n=========" + str(count) + "========")
 
-        print(buffer)
-        print(tokens_type)
+            left_type = self.get_token_type(self.stack[-1])
+            right_type = self.get_token_type(self.buffer[0])
+            (action, relation) = self.get_action_and_relation()
+            if relation and (action != SHIFT or action != REDUCE):
+                self.malt.update(relation)
+
+            self.execute_action(action)
+
+            # print(left_type, right_type, "=>", action, relation)
+            # print(self.buffer)
+            # print(self.stack)
+            # count += 1
+
+        del self.stack
+        del self.buffer
+
+        return
 
     def get_malt(self):
         return self.malt
@@ -55,3 +79,48 @@ class MaltParser():
         elif word == ROOT:
             return ROOT
         return None
+
+    def get_action_and_relation(self):
+        # stack head
+        left = self.stack[-1]
+        left_type = self.get_token_type(left)
+
+        # buffer first element
+        right = self.buffer[0]
+        right_type = self.get_token_type(right)
+
+        relation_result = {}
+
+        for relation_name, relation_list in self.dependency_relations.items():
+            if (left_type, right_type) in relation_list:
+                relation_result = {
+                    (left, right): relation_name
+                }
+                return (RIGHTARC, relation_result)
+            elif (right_type, left_type) in relation_list:
+                relation_result = {
+                    (right, left): relation_name
+                }
+                return (LEFTARC, relation_result)
+
+        # Checking whether REDUCE or SHIFT
+        if len(self.buffer) == 0:
+            return (REDUCE, None)
+
+        relations = self.malt.keys()
+        if len(self.stack) > 1:
+            stack_head = self.stack[-1]
+            stack_head_under = self.stack[-2]
+            pair = (stack_head, stack_head_under)
+            pair_reverse = (stack_head_under, stack_head)
+            if pair in relations or pair_reverse in relations:
+                return(REDUCE, None)
+
+        return (SHIFT, None)
+
+    def execute_action(self, action):
+        if action == LEFTARC or action == REDUCE:
+            self.stack.pop()
+        elif action == RIGHTARC or action == SHIFT:
+            self.stack.append(self.buffer[0])
+            self.buffer.pop(0)
