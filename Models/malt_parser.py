776cf7c9f2cb5f19de375c1517e968066952158b
Nhan Nguyen <trinhan194@gmail.com>
2021-06-13 12:11:42 +0700

    Finish get_token_type

--- Models/malt_parser.py
+++ Models/malt_parser.py
@@ -1,3 +1,4 @@
+from posixpath import lexists
 import re
 from copy import copy
 from pyvi import ViTokenizer
@@ -8,6 +9,7 @@ def malt_parser(str, my_lexicals, rules):
     sentence = copy(str)
     tokens = ViTokenizer.tokenize(sentence).lower().split()
     tokens = handle_splitting_xebus(tokens)
+    tokens_type = list(map(lambda x: get_token_type(x, my_lexicals), tokens))
 
     return malt
 
@@ -21,4 +23,11 @@ def handle_splitting_xebus(tokens):
 
 
 def get_token_type(word, my_lexicals):
+    for token_type in my_lexicals:
+        token_type_list = list(
+            map(lambda x: x.lower(), my_lexicals[token_type]))
+        if word.lower() in token_type_list:
+            return token_type
+    if re.search("\d{3,4}hr", word):
+        return "time"
     return None
