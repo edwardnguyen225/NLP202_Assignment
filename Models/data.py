79cb0a5bcfdddd0dacaabbe5c14694a8cfeda04d
Nhan Nguyen <trinhan194@gmail.com>
2021-06-15 11:13:30 +0700

    Add question; Fix dependencies relations

--- Models/data.py
+++ Models/data.py
@@ -7,6 +7,7 @@ ROOT = "<ROOT>"
 TIME_MODE = "TIME-MODE"
 
 DEFAULT_QUESTIONS = [
+    "Xe bus nào từ thành phố Hồ Chí Minh lúc 10:00HR đến thành phố Huế lúc 19:00HR ?",
     "Xe bus nào đến thành phố Huế lúc 20:00HR ?",
     "Thời gian xe bus B3 từ Đà Nẵng đến Huế ?",
     "Xe bus nào đến thành phố Hồ Chí Minh ?",
@@ -34,11 +35,10 @@ my_lexicals = {
     "BUS": ["bus", "buýt", "xe_bus", "xe_buýt", "xe"],
     "CITY": ["thành_phố", "tỉnh"],
     "BUS-ARRIVE": ["đến", "tới"],
-    "BUS-SRC": ["từ"],
+    "FROM": ["từ"],
     "WH": ["nào"],
     "WH-TIME": ["lúc_nào"],
-    "BUS-ATIME": ["lúc", "vào_lúc"],
-    "BUS-DTIME": ["lúc", "từ_lúc"],
+    "POINT-TIME": ["lúc", "vào_lúc"],
     "BUS-RUNTIME": ["thời_gian"],
     "BUS-CODE": ["B1", "B2", "B3", "B4", "B5", "B6"],
     "CITY-NAME": ["hue", "hcm", "hn", "danang", "hồ_chí_minh", "hà_nội", "huế", "đà_nẵng"],
@@ -47,15 +47,13 @@ my_lexicals = {
 
 # dependency from left --> right
 dependency_relations = {
-    "PRED": [("<ROOT>", "BUS-ARRIVE")],
+    "PRED": [(ROOT, "BUS-ARRIVE")],
     "LSUBJ": [("BUS-ARRIVE", "BUS")],
-    "PREP": [("BUS-ARRIVE", "BUS-SRC")],
-    "FROM-TIME": [
-        ("BUS-SRC", TIME_MODE),
-        ("BUS-DTIME", TIME_MODE)],
-    "TO-TIME": [("BUS-ATIME", TIME_MODE)],
-    "PREP-TIME": [("BUS-ARRIVE", "BUS-ATIME")],
-    "FROM-LOC": [("BUS-SRC", "CITY"), ("BUS-SRC", "CITY-NAME")],
+    "PREP": [("BUS-ARRIVE", "FROM")],
+    "AT-TIME": [("FROM", TIME_MODE), ("POINT-TIME", TIME_MODE)],
+    "FROM-TIME": [("FROM", "POINT-TIME")],
+    "PREP-TIME": [("BUS-ARRIVE", "POINT-TIME")],
+    "FROM-LOC": [("FROM", "CITY"), ("FROM", "CITY-NAME")],
     "TO-LOC": [("BUS-ARRIVE", "CITY"), ("BUS-ARRIVE", "CITY-NAME")],
     "CITY-NP": [("CITY", "CITY-NAME")],
     "BUS-NP": [("BUS", "BUS-CODE")],
@@ -64,8 +62,6 @@ dependency_relations = {
         ("BUS-ARRIVE", "BUS-RUNTIME"),
         ("BUS-RUNTIME", "WH")],
     "WH-TIME": [("BUS-ARRIVE", "WH-TIME")],
-    # "WH-FROM-TIME": [("BUS-DTIME", "WH"), ("BUS-ARRIVE", "WH")],
-    # "WH-TO-TIME": [("BUS-ATIME", "WH")],
     "WH-CITY": [("CITY", "WH")],
 }
 
