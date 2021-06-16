8852686f569a77c58e200610e42251ce8625bfe0
Nhan Nguyen <trinhan194@gmail.com>
2021-06-16 22:15:12 +0700

    Add result for testing, city dict, get_city_realname for getting result

--- Models/data.py
+++ Models/data.py
@@ -4,17 +4,21 @@ from re import search
 from Models.bus import Bus
 
 ROOT = "<ROOT>"
+GAP = "<GAP>"
 TIME_MODE = "TIME-MODE"
-
-DEFAULT_QUESTIONS = [
-    "Xe bus nào từ Hồ Chí Minh lúc 10:00HR đến thành phố Huế lúc 19:00HR ?",
-    "Xe bus nào đến thành phố Huế lúc 20:00HR ?",
-    "Thời gian xe bus B3 từ Đà Nẵng đến Huế ?",
-    "Xe bus nào đến thành phố Hồ Chí Minh ?",
-    "Xe bus nào đến thành phố Hồ Chí Minh lúc 20:00HR ?",
-    "Xe bus B3 từ Đà Nẵng lúc 2030HR đến thành phố nào ?",
-    "Xe bus B3 từ Đà Nẵng đến Huế lúc nào ?"
-]
+DEFAULT_QUESTIONS = {
+    "Xe bus nào từ Hồ Chí Minh lúc 10:00HR đến thành phố Huế lúc 19:00HR ?": ["B1"],
+    "Xe bus nào đến thành phố Huế lúc 20:00HR ?": ["B3"],
+    "Thời gian xe bus B3 từ Đà Nẵng đến Huế ?": "4:00HR",
+    "Thời gian xe bus B4 từ Đà Nẵng đến Huế ?": "Sai địa điểm khởi hành/kết thúc của xe bus B4",
+    "Thời gian xe bus từ Huế đến Hanoi ?": ["16:00HR"],
+    "Xe bus nào đến thành phố Hồ Chí Minh ?": ["B4"],
+    "Xe bus nào đến HUE ?": ["B1", "B3", "B2"],
+    "Xe bus nào đến từ HCMC ?": ["B1", "B2"],
+    "Xe bus B3 đến từ thành phố nào ?": "Đà Nẵng",
+    "Xe bus B3 từ Đà Nẵng đến Huế lúc nào ?": "20:00HR",
+    "Xe bus từ Đà Nẵng lúc 5:30HR đến thành phố nào ?": "Hà Nội",
+}
 
 REDUNDANT_TOKENS = [".", ",", "?", ":"]
 
@@ -31,6 +35,16 @@ PATH_TO_OUTPUT_FILES = {
     "f": join(PATH_TO_OUTPUT, "output_f.txt"),
 }
 
+CITY_NAME_DICTIONARY = {
+    "Hồ Chí Minh": ["hồ chí minh", "ho chi minh", "hồ_chí_minh", "ho_chi_minh", "hcm", "hcmc", "hochiminh"],
+    "Huế": ["huế", "hue"],
+    "Hà Nội": ["hà nội", "ha noi", "hà_nội", "ha_noi", "hn", "hanoi"],
+    "Đà Nẵng": ["đà nẵng", "da nang", "đà_nẵng", "da_nang", "dn", "danang"]
+}
+
+CITY_NAMES = [name for subnames in list(
+    CITY_NAME_DICTIONARY.values()) for name in subnames]
+
 my_lexicals = {
     "BUS": ["bus", "buýt", "xe_bus", "xe_buýt", "xe"],
     "CITY": ["thành_phố", "tỉnh"],
@@ -41,7 +55,7 @@ my_lexicals = {
     "POINT-TIME": ["lúc", "vào_lúc"],
     "BUS-RUNTIME": ["thời_gian"],
     "BUS-CODE": ["B1", "B2", "B3", "B4", "B5", "B6"],
-    "CITY-NAME": ["hue", "hcm", "hn", "danang", "hồ_chí_minh", "hà_nội", "huế", "đà_nẵng"],
+    "CITY-NAME": CITY_NAMES,
     TIME_MODE: [TIME_MODE]
 }
 
@@ -137,6 +151,7 @@ def set_arrival(lst):
         raise Exception("Unknown bus code: %s" % bus_code)
 
     bus = BUS_DATA[bus_code]
+    dest = get_city_realname(dest)
     bus.set_dest(dest)
     bus.set_atime(atime)
 
@@ -147,6 +162,7 @@ def set_departure(lst):
         raise Exception("Unknown bus code: %s" % bus_code)
 
     bus = BUS_DATA[bus_code]
+    src = get_city_realname(src)
     bus.set_src(src)
     bus.set_dtime(dtime)
 
@@ -157,6 +173,8 @@ def set_runtime(lst):
         lst.pop(-1)
 
     bus_code, src, dest, runtime = lst
+    src = get_city_realname(src)
+    dest = get_city_realname(dest)
     if bus_code not in BUS_DATA:
         raise Exception("Unknown bus code: %s" % bus_code)
 
@@ -172,6 +190,15 @@ def set_runtime(lst):
 def print_buses():
     for bus in BUS_DATA:
         print(BUS_DATA[bus])
+
+
+def get_city_realname(city):
+    if city is None:
+        return None
+    for city_realname in CITY_NAME_DICTIONARY:
+        if city.lower() in CITY_NAME_DICTIONARY[city_realname]:
+            return city_realname
+    return city
 
 
 load_bus_data()
